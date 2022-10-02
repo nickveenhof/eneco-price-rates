165397ad8c05ffd93fdfc9625be7ab29920078b4
Thijs Vermeir <thijsvermeir@gmail.com>
2022-10-02 15:16:29 +0200

    sensor: add multiple sensors in platform

--- custom_components/eneco_price_rates/sensor.py
+++ custom_components/eneco_price_rates/sensor.py
@@ -17,14 +17,19 @@ def setup_platform(
     # We only want this platform to be set up via discovery.
     if discovery_info is None:
         return
-    add_entities([EnergyPriceSensor('Energiekost Dagtarief', 0.2627)])
+    add_entities([
+        ElectricityPriceSensor('Energiekost Dagtarief', 0.2627),
+        ElectricityPriceSensor('Energiekost Nachttarief', 0.2598),
+        GasPriceSensor('Energiekost gasprijs', 0.1122),
+    ])
 
 
 class EnergyPriceSensor(SensorEntity):
-    """Representation of a sensor."""
+    """Representation of an energy price sensor."""
 
     def __init__(self, name: str, value: float) -> None:
         """Initialize the sensor."""
+        super().__init__()
         self._name = name
         self._state = value
 
@@ -54,3 +59,29 @@ class EnergyPriceSensor(SensorEntity):
         This is the only method that should fetch new data for Home Assistant.
         """
         pass
+
+
+class ElectricityPriceSensor(EnergyPriceSensor):
+    """Representation of an electricity price sensor."""
+
+    def __init__(self, name: str, value: float) -> None:
+        """Initialize the sensor."""
+        super().__init__(name, value)
+
+    @property
+    def icon(self) -> str:
+        """Return the icon of the sensor."""
+        return "mdi:lightning-bolt"
+
+
+class GasPriceSensor(EnergyPriceSensor):
+    """Representation of an gas price sensor."""
+
+    def __init__(self, name: str, value: float) -> None:
+        """Initialize the sensor."""
+        super().__init__(name, value)
+
+    @property
+    def icon(self) -> str:
+        """Return the icon of the sensor."""
+        return "mdi:barrel"
